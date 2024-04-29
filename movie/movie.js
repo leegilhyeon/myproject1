4d4b05eef10e873a370a90710b840730cdec3142
leegilhyeon <164996803+leegilhyeon@users.noreply.github.com>
2024-04-29 13:56:33 +0900

    Add files via upload

--- movie/movie.js
+++ movie/movie.js
@@ -1,11 +1,76 @@
-const movieBoxs = document.getElementById('movie-boxs') //id
-const creatMovieCard = movie => {
-  const {id, title, overveiw,poster_path, vote_average} = movie;
+
+// api 데이터 불러오기
+
+function apidata() {
+  const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NmYwN2U4NzM1YzE5MWVkMzY1YzcxZjhiOTJmNjk0MiIsInN1YiI6IjY2MjhkZDkyMzk1NDlhMDE2NjAxMmQzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MILDD_lRkROvvW5HDA4GnvUdw_ZxTRgtEmnZXSiy-Wk'
+    }
+};
+fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',options)
+    .then(response => response.json())
+    .then(response => {
+      moviecard(response.results);
+    })
+   .catch(err=>  console.log(err));
+   
+
 }
+//api데이터로 영화카드 만들기
 
+const maincard = document.createElement('div');
+const movieBoxs = document.getElementById('cards') 
+maincard.classList.add('cards')
 
+function moviecard(movie) {
+  movieBoxs.innerHTML=''; 
+  
+  movie.forEach(movie => {
+    let title = movie.title;
+    let overview = movie.overview;
+    let poster = movie.poster_path;
+    let vote = movie.vote_average;
+    let id = movie.id;
 
-const options = {
+    const temp_html = `
+  <div class="col" onclick = "alert('영화 id: ${id}')">       
+  <img src="https://image.tmdb.org/t/p/w500${poster}" alt="..." class="posterimg">
+    <h5 class="title">${title}</h5>
+    <p class="overview">${overview}</p>
+    <p class="vote_average">평점: ${vote}</p>
+     </div>`
+  
+movieBoxs.insertAdjacentHTML('beforeend',temp_html);
+  });
+
+};
+
+//검색기능
+const inputBox = document.querySelector('#search-input');
+const searchBtn = document.querySelector('.searchbtn')
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  apidata();
+}) //페이지로드하면 이벤트실행
+ 
+document.addEventListener('keydown',function (event) {
+  if(event.key === 'Enter') {
+    searchPost();
+  }
+});
+
+inputBox.focus();
+
+window.searchPost = () => {
+  let searchmv = inputBox.value.trim();
+  if (searchmv === '') {
+    alert('검색어를 입력해주세요');
+    inputBox.focus();
+    return;
+  }
+  const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
@@ -13,31 +78,13 @@ const options = {
     }
 };
 
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',options)
     .then(response => response.json())
     .then(response => {
-        
-        let movies = response["results"];
-            movies.forEach(a => {
-              let title = a['original_title']
-              let overveiw = a['overview']
-              let vote_average = a['vote_average']
-              let poster_path = a['poster_path']
-
-              let temp_html = `           
-            <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-          <div class="card">
-            <img src="..." class="poster_path" alt="...">
-            <div class="card-body">
-              <h5 class="title">${title}</h5>
-              <p class="overveiw">${overview} </p>
-              <p class="vote_average">평점은:${vote_average}</p>
-               `
-              $('#movie-boxs').append(temp_html);
-            })
-    })
-    
-    
-    
-    .catch(err => console.error(err));
+  if (response.results.length > 0 ) {
+    moviecard (response.results);
+  } else { alert('검색 결과가 없습니다'); return;}
+})
+   .catch(err=>  console.log(err));
+}
+ 
